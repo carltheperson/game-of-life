208bafd3d89603b6cb7de86f1655e28811aa2416
Carl Riis <carltheperson@protonmail.com>
2021-06-10 10:18:35 +0200

    Basic game working

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func generateBlocks(w int, l int) [][]*imdraw.IMDraw {
 			coordY := float64(j)*blockSize + float64(j)*padding
 			imd := imdraw.New(nil)
 			imd.Circle(coordX, coordY)
-			imd.Color = pixel.RGB(1, 0, 0)
+			imd.Color = pixel.RGB(0, 0, 0)
 			imd.Ellipse(pixel.V(120, 80), 0)
 			imd.Push(pixel.V(coordX, coordY))
 			imd.Push(pixel.V(coordX+blockSize, coordY))
@@ -40,6 +40,16 @@ func generateMatrix(w int, l int) [][]bool {
 	x := make([][]bool, w)
 	for i := 0; i < w; i++ {
 		y := make([]bool, l)
+		x[i] = y
+	}
+	return x
+}
+
+func generateRandomMatrix(w int, l int) [][]bool {
+	rand.Seed(time.Now().Unix())
+	x := make([][]bool, w)
+	for i := 0; i < w; i++ {
+		y := make([]bool, l)
 		for j := 0; j < l; j++ {
 			if rand.Intn(2) == 1 {
 				y[j] = true
@@ -50,6 +60,13 @@ func generateMatrix(w int, l int) [][]bool {
 		x[i] = y
 	}
 	return x
+}
+
+func checkNeighbor(i int, j int, matrix [][]bool) bool {
+	if i >= 0 && j >= 0 && i < len(matrix) && j < len(matrix[0]) {
+		return matrix[i][j]
+	}
+	return false
 }
 
 func run() {
@@ -64,9 +81,11 @@ func run() {
 	}
 
 	blocks := generateBlocks(50, 60)
-	matrix := generateMatrix(50, 60)
+	matrix := generateRandomMatrix(50, 60)
 
-	fps := 1
+	neighborCoords := [][]int{{-1, -1}, {0, -1}, {1, -1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
+
+	fps := 10
 	timeForOneFrameMilliseconds := (1.0 / float64(fps)) * 1000
 
 	start := time.Now()
@@ -77,11 +96,29 @@ func run() {
 		elapsed := float64(t.Sub(start).Milliseconds())
 		if elapsed > timeForOneFrameMilliseconds {
 			start = time.Now()
+			newMatrix := generateMatrix(50, 60)
 			for i := range matrix {
 				for j := range matrix[0] {
-					matrix[i][j] = !matrix[i][j]
+					numberOfNeighbors := 0
+					for _, coords := range neighborCoords {
+						if checkNeighbor(i+coords[0], j+coords[1], matrix) {
+							numberOfNeighbors += 1
+						}
+					}
+
+					if matrix[i][j] && numberOfNeighbors < 2 {
+						newMatrix[i][j] = false
+					} else if !matrix[i][j] && numberOfNeighbors == 3 {
+						newMatrix[i][j] = true
+					} else if matrix[i][j] && numberOfNeighbors > 3 {
+						newMatrix[i][j] = false
+					} else {
+						newMatrix[i][j] = matrix[i][j]
+					}
+
 				}
 			}
+			matrix = newMatrix
 		}
 
 		for i, yBlocks := range blocks {
